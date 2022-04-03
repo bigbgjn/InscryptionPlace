ddef712474fb118575875b91cce9e449117afe72
extszombiemaster <EXTSZombiemaster786@gmail.com>
2022-04-03 17:34:26 -0600

    Update userscript.user.js

--- userscript.user.js
+++ userscript.user.js
@@ -16,8 +16,8 @@ if (window.top !== window.self) {
                 Date.now = function() { return new Date().getTime(); }
             }
             const i = document.createElement("img");
-            i.src = "https://cdn.discordapp.com/attachments/413985144219893760/960222828815347782/indiesOverlayInscryption3-3000x6000.png";
-            i.style = "position: absolute;left: 0;top: 0px;image-rendering: pixelated;width: 2000px;height: 1000px;";
+            i.src = "https://cdn.discordapp.com/attachments/913552640758128702/960321025399197736/aaa.png";
+            i.style = "position: absolute;left: 0;top: 0px;image-rendering: pixelated;width: 2000px;height: 2000px;";
             i.id = "mcss-overlay"
             i.setAttribute("vis",1)
             console.log(i);
